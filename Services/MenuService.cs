939fa4830d86e1815d1dec832fe2b91af5f437b2
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-10-04 16:19:33 -0600

    Setup()

--- Services/MenuService.cs
+++ Services/MenuService.cs
@@ -5,7 +5,17 @@ namespace BurgerShack.Services
 {
     public class MenuService
     {
+        public List<MenuItem> MenuItems { get; private set; }
+        public List<string> Messages { get; set; }
 
+        public void Setup()
+        {
+            Entree nBurger = new Entree("Nathan Burger", 6, "A burger for the discerning gourmand.");
+            Entree dBurger = new Entree("Dory Burger", 3, "Half of a Nathan Burger.  The other half has been eaten by Nathan.");
+            Entree chicken = new Entree("Chicken Sandwich", 4, "Like a burger, but with chicken.  So not really a burger at all.");
+
+            MenuItems.AddRange(new MenuItem[] { nBurger, dBurger, chicken });
+        }
 
 
 
@@ -13,7 +23,9 @@ namespace BurgerShack.Services
 
         public MenuService()
         {
-
+            MenuItems = new List<MenuItem>();
+            Messages = new List<string>();
+            Setup();
         }
     }
 }
